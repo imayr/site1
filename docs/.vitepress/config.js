f9c4ac01ddc6324ce9e301b8a342d191a332c9d3
Ingemar Mayr <Ingemar.Mayr@itelio.com>
2022-03-31 15:41:39 +0200

    test

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Arcsecond Oort Uploader',
+  description: 'The open-source easy-to-use tool for uploading data to Arcsecond.io.',
+  base: '/oort/',
+  themeConfig: {
+    nav: [{ text: 'Arcsecond Docs', link: 'https://docs.arcsecond.io' }]
+  }
+}
+
